a333d063234c5586f564ad53fb8caabc373b08c7
William Hendershot <whendershot1@gmail.com>
2022-02-08 09:19:12 +0900

    Initial implementation

---
+++ Week1/Extra Practice/Hello World/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("My name is William.");
+        System.out.println("I am middle aged. ;)");
+        System.out.println("My hometown is Seattle, WA, USA.");
+    }
+}
