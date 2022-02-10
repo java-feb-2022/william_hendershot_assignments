d690b2ca3c81bd685b38cbc83396b34e5699bddb
William Hendershot <whendershot1@gmail.com>
2022-02-10 15:58:49 +0900

    Initial implementation

---
+++ week1/extra_practice/list_of_exceptions/ExceptionList.java
@@ -0,0 +1,24 @@
+package week1.extra_practice.list_of_exceptions;
+
+import java.util.ArrayList;
+
+public class ExceptionList {
+    
+    public static void main(String[] args){
+        ArrayList<Object> myList = new ArrayList<Object>();
+        myList.add("13");
+        myList.add("hello world");
+        myList.add(48);
+        myList.add("Goodbye World");
+
+        for(int i = 0; i < myList.size(); i++) {
+            try {
+                Integer castedValue = (Integer) myList.get(i);
+                System.out.println(castedValue);
+            }
+            catch (ClassCastException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+}
