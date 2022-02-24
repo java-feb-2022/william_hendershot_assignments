78e8392d03a2b406bd439b4d872f5cd65e834626
William Hendershot <whendershot1@gmail.com>
2022-02-24 09:51:53 +0900

    Challenge setup

---
+++ week3/extra_practice/hoppers_receipt/src/test/java/com/codingdojo/hoppers_receipt/HoppersReceiptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.hoppers_receipt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoppersReceiptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
