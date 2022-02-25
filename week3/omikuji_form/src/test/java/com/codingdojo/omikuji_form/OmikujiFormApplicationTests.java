a6a2a5b3d3eede8ece8e75da65bb7f83849aa866
William Hendershot <whendershot1@gmail.com>
2022-02-25 16:34:33 +0900

    Challenge setup

---
+++ week3/omikuji_form/src/test/java/com/codingdojo/omikuji_form/OmikujiFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.omikuji_form;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmikujiFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
