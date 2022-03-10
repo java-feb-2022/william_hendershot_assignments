09a42bdcc727cd1310d0497149f4b4a68fbbed10
William Hendershot <whendershot1@gmail.com>
2022-03-10 10:52:08 +0900

    Add missing annotation

--- week5/dojos_and_ninjas/src/main/java/com/codingdojo/dojos_and_ninjas/mvc/repositories/NinjaRepository.java
+++ week5/dojos_and_ninjas/src/main/java/com/codingdojo/dojos_and_ninjas/mvc/repositories/NinjaRepository.java
@@ -5,7 +5,9 @@ import java.util.List;
 import com.codingdojo.dojos_and_ninjas.mvc.models.Ninja;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface NinjaRepository extends CrudRepository<Ninja, Long> {
     
     public List<Ninja> findAll();
