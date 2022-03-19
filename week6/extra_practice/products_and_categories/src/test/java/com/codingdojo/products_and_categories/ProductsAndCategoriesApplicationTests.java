11b318f48b5f58ff6818ea757acbade81eba1c9b
William Hendershot <whendershot1@gmail.com>
2022-03-19 10:08:12 +0900

    Challenge setup

---
+++ week6/extra_practice/products_and_categories/src/test/java/com/codingdojo/products_and_categories/ProductsAndCategoriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.products_and_categories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsAndCategoriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
